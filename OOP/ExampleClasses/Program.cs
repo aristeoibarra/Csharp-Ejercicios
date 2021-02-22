d455de921fd3ba9d56c4e9f67f79d7712d2b3485
AristeoIbarra <aristeoibarra608@gmail.com>
2021-02-22 09:33:27 -0700

    OOP concepts

---
+++ OOP/ExampleClasses/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExampleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("Aristeo", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance:C} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance.ToString("C"));
+
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me");
+            Console.WriteLine(account.Balance.ToString("C"));
+
+            // Test that the initial balances must be positive.
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.Message);
+            }
+
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.Message);
+            }
+
+            // Get the account history
+            Console.WriteLine();
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
---
+++ OOP/ExampleClasses/Transaction.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ExampleClasses
+{
+    public class Transaction
+    {
+        // Properties
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        // Constructor 
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
